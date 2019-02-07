61b767224c5f4dc81f619f3303456257f55e799e
bellzebu <bellzebu@hotmail.com>
2019-02-07 21:17:12 +0100

    toEqual() => toBe()

--- spec/linter-gfortran-spec.js
+++ spec/linter-gfortran-spec.js
@@ -14,16 +14,16 @@ describe('The Fortran provider for AtomLinter', () => {
   it('finds an error in "error.f95"', () => {
     waitsForPromise(() => atom.workspace.open(path.join(FILES, 'error.f95')).then(editor =>
       lint(editor).then((messages) => {
-        expect(messages.length).toEqual(1);
-        expect(messages[0].severity).toEqual('error');
-        expect(messages[0].excerpt).toEqual('Unterminated character constant beginning at (1)');
+        expect(messages.length).toBe(1);
+        expect(messages[0].severity).toBe('error');
+        expect(messages[0].excerpt).toBe('Unterminated character constant beginning at (1)');
       })));
   });
 
   it('works with modules in "module.f95"', () => {
     waitsForPromise(() => atom.workspace.open(path.join(FILES, 'module.f95')).then(editor =>
       lint(editor).then((messages) => {
-        expect(messages.length).toEqual(0);
+        expect(messages.length).toBe(0);
       })));
   });
 });
