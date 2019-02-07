9caa5876093890383a7a4d7ca6c12c510ecf49b0
bellzebu <bellzebu@hotmail.com>
2019-02-07 18:51:51 +0100

    Support v2 Linter API

--- spec/linter-gfortran-spec.js
+++ spec/linter-gfortran-spec.js
@@ -2,11 +2,11 @@
 
 import * as path from 'path';
 
+const { lint } = require('../lib/main.js').provideLinter();
+
 const FILES = path.join(__dirname, 'files');
 
 describe('The Fortran provider for AtomLinter', () => {
-  const lint = require('../lib/main').provideLinter().lint;
-
   beforeEach(() => {
     waitsForPromise(() => atom.packages.activatePackage('linter-gfortran'));
   });
@@ -15,17 +15,15 @@ describe('The Fortran provider for AtomLinter', () => {
     waitsForPromise(() => atom.workspace.open(path.join(FILES, 'error.f95')).then(editor =>
       lint(editor).then((messages) => {
         expect(messages.length).toEqual(1);
-        expect(messages[0].type).toEqual('Error');
-        expect(messages[0].text).toEqual('Unterminated character constant beginning at (1)');
-      }),
-    ));
+        expect(messages[0].severity).toEqual('error');
+        expect(messages[0].excerpt).toEqual('Unterminated character constant beginning at (1)');
+      })));
   });
 
   it('works with modules in "module.f95"', () => {
     waitsForPromise(() => atom.workspace.open(path.join(FILES, 'module.f95')).then(editor =>
       lint(editor).then((messages) => {
         expect(messages.length).toEqual(0);
-      }),
-    ));
+      })));
   });
 });
